fa8d12183cf3d553eda8d716b474c7c0d7431b0d
Cory Shain <cory.shain@gmail.com>
2022-11-08 13:27:24 -0500

    Fix to batch script generation

--- cdrxl/bin/make_jobs.py
+++ cdrxl/bin/make_jobs.py
@@ -23,10 +23,10 @@ if __name__ == '__main__':
     ''')
     argparser.add_argument('paths', nargs='+', help='Path(s) to CDRXL config file(s).')
     argparser.add_argument('-j', '--jobtype', default='train', help='Type of job, one of "train", "eval".')
-    argparser.add_argument('-t', '--time', type=int, default=48, help='Maximum number of hours to train models')
+    argparser.add_argument('-t', '--time', type=int, default=24, help='Maximum number of hours to train models')
     argparser.add_argument('-n', '--n_cores', type=int, default=4, help='Number of cores to request')
     argparser.add_argument('-g', '--use_gpu', action='store_true', help='Whether to request a GPU node')
-    argparser.add_argument('-m', '--memory', type=int, default=64, help='Number of GB of memory to request')
+    argparser.add_argument('-m', '--memory', type=int, default=16, help='Number of GB of memory to request')
     argparser.add_argument('-P', '--slurm_partition', default=None, help='Value for SLURM --partition setting, if applicable')
     argparser.add_argument('-e', '--exclude', nargs='+', help='Nodes to exclude')
     argparser.add_argument('-s', '--singularity_path', default='', help='Path to singularity image to invoke before running')
@@ -54,31 +54,27 @@ if __name__ == '__main__':
         with open(path, 'r') as f:
             c = yaml.load(f, Loader=Loader)
 
-        niter = c['niter']
-        nfolds = c['nfolds']
         model_outdir = os.path.normpath(c['outdir'])
         if not os.path.exists(model_outdir):
             os.makedirs(model_outdir)
 
-        for iteration in range(niter):
-            for fold in range(nfolds):
-                job_name = os.path.basename(path)[:-4] + '_i%d_f%d' % (iteration+1, fold+1)
-                filename = os.path.join(outdir, job_name + '.pbs')
-                with open(filename, 'w') as f:
-                    f.write(base % (job_name, job_name, time, memory, n_cores))
-                    if use_gpu:
-                        f.write('#SBATCH --gres=gpu:1\n')
-                    if slurm_partition:
-                        f.write('#SBATCH --partition=%s\n' % slurm_partition)
-                    if exclude:
-                        f.write('#SBATCH --exclude=%s\n' % exclude)
-                    wrapper = '%s'
-                    if singularity_path:
-                        if use_gpu:
-                            wrapper = wrapper % ('singularity exec --nv %s bash -c "cd %s; %%s"\n' % (singularity_path, os.getcwd()))
-                        else:
-                            wrapper = wrapper % ('singularity exec %s bash -c "cd %s; %%s"\n' % (singularity_path, os.getcwd()))
+        job_name = os.path.basename(path)[:-4]
+        filename = os.path.join(outdir, job_name + '.pbs')
+        with open(filename, 'w') as f:
+            f.write(base % (job_name, job_name, time, memory, n_cores))
+            if use_gpu:
+                f.write('#SBATCH --gres=gpu:1\n')
+            if slurm_partition:
+                f.write('#SBATCH --partition=%s\n' % slurm_partition)
+            if exclude:
+                f.write('#SBATCH --exclude=%s\n' % exclude)
+            wrapper = '%s'
+            if singularity_path:
+                if use_gpu:
+                    wrapper = wrapper % ('singularity exec --nv %s bash -c "cd %s; %%s"\n' % (singularity_path, os.getcwd()))
+                else:
+                    wrapper = wrapper % ('singularity exec %s bash -c "cd %s; %%s"\n' % (singularity_path, os.getcwd()))
 
-                    job_str = wrapper % ('python3 -m cdrxl.%s %s %s %s' % (jobtype, path, iteration+1, fold+1))
-                    f.write(job_str)
+            job_str = wrapper % ('python3 -m cdrxl.bin.%s %s' % (jobtype, path))
+            f.write(job_str)
 
